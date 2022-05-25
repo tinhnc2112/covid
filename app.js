112868b8e72a9556415f5815cba0df57e0d1e0a0
tinhnc2811 <tinhnc2811@gmail.com>
2022-05-25 15:19:02 +0700

    v3

--- app.js
+++ app.js
@@ -1,148 +1,183 @@
-
-// const api = 'https://api.covidtracking.com/v1/us/daily.json'
-// var fetchDatas = []
-// function fetchData() {
-//     fetch(api)
-//         .then(function (response) {
-//             return response.json()
-//         })
-//         .then(function (data) {
-//             fetchDatas = data.map(({ positive, recovered, death, dateChecked: date }) => ({
-//                 positive,
-//                 recovered,
-//                 death,
-//                 date
-//             }));
-//             console.log("fdj");
-//             console.log(fetchDatas);
-//             const datas = {
-//                 labels: fetchDatas.map(({ date }) =>
-//                     new Date(date).toLocaleDateString(),
-//                 ),
-//                 datasets: [
-//                     {
-//                         data: fetchDatas.map((data) => data.positive),
-//                         borderColor: '#3333ff',
-//                         label: 'Infected',
-//                         fill: true,
-//                     }, {
-//                         data: fetchDatas.map((data) => data.death),
-//                         borderColor: 'red',
-//                         backgroundColor: "rgba(255, 0, 0, 0.5)",
-//                         label: 'Deaths',
-//                         fill: true,
-//                     }, {
-//                         data: fetchDatas.map((data) => data.recovered),
-//                         borderColor: 'green',
-//                         backgroundColor: "rgba(0, 255, 0, 0.5)",
-//                         label: 'Recovered',
-//                         fill: true,
-//                     }
-//                 ]
-//             };
-
-//             const config = {
-//                 type: 'line',
-//                 data: datas,
-//                 options: {}
-//             };
-//             const myChart = new Chart(
-//                 document.getElementById('myChart'),
-//                 config
-//             );
-//         })
-// }
-// fetchData()
-
 const apiDetail = 'https://api.covidtracking.com/v1/us/daily.json'
 const apiCommon = 'https://covid19.mathdro.id/api'
+var countryData = ''
 var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-const fetchDataCommon = async () => {
-    let response = await fetch(apiCommon)
-    let dataCommon = await response.json()
-    return dataCommon
+const fetchDataCommon = async (country) => {
+    if (country) {
+        let response = await fetch(`${apiCommon}/countries/${country}`)
+        let dataCommon = await response.json()
+        return dataCommon
+    } else {
+        let response = await fetch(apiCommon)
+        let dataCommon = await response.json()
+        return dataCommon
+    }
 }
+// 
+const renderCards = (item) => {
+    const html = `
+    <p>${item.title}</p>
+    <p style="font-size:24px">${item.value}</p>
+    <p>${item.date}</p>
+    <p>${item.desc}</p>
+    `
+    document.querySelector(item.selector).innerHTML = html
+}
+
+
 fetchDataCommon().then(dataCommon => {
     const recoveredValue = new Intl.NumberFormat().format(dataCommon.recovered.value)
     const infectedValue = new Intl.NumberFormat().format(dataCommon.confirmed.value)
-    const deadthsValue = new Intl.NumberFormat().format(dataCommon.deaths.value)
+    const deathsValue = new Intl.NumberFormat().format(dataCommon.deaths.value)
     const dateChecked = new Date(dataCommon.lastUpdate).toLocaleDateString("en-US", options)
-    let htmlRecovered = `<p>Recovered</p>
-    <p style="font-size:24px">${recoveredValue}</p>
-    <p>${dateChecked}</p>
-    <p>Number of recoveries from COVID-19.</p>
-    `
-    let htmlInfected = `<p>Infected</p>
-    <p style="font-size:24px">${infectedValue}</p>
-    <p>${dateChecked}</p>
-    <p>Number of recoveries from COVID-19.</p>
-    `
-    let htmlDeaths = `<p>Deaths</p>
-    <p style="font-size:24px">${deadthsValue}</p>
-    <p>${dateChecked}</p>
-    <p>Number of recoveries from COVID-19.</p>
-    `
-    document.querySelector('.recovered').innerHTML = htmlRecovered
-    document.querySelector('.infected').innerHTML = htmlInfected
-    document.querySelector('.deaths').innerHTML = htmlDeaths
+    const cards = {
+        recovered: {
+            title: 'Recovered',
+            value: recoveredValue,
+            date: dateChecked,
+            desc: 'Number of recoveries from COVID-19.',
+            selector: '.recovered'
+        }, infected: {
+            title: 'Infected',
+            value: infectedValue,
+            date: dateChecked,
+            desc: 'Number of active cases from COVID-19.',
+            selector: '.infected'
+        }, deaths: {
+            title: 'Deaths',
+            value: deathsValue,
+            date: dateChecked,
+            desc: 'Number of deaths caused by COVID-19.',
+            selector: '.deaths'
+        }
+    }
 
+    renderCards(cards.recovered);
+    renderCards(cards.infected);
+    renderCards(cards.deaths);
 })
+
 const fetchCountries = async () => {
-    let response = await fetch(apiCommon + '/countries')
+    let response = await fetch(`${apiCommon}/countries`)
     let dataCountries = await response.json()
     return dataCountries
 }
 
 fetchCountries().then(countries => {
-    debugger
     let html = countries.countries.map((country) =>
-        `<option>${country.name}</option>`,
+        `<option value="${country.name}">${country.name}</option>`,
     )
-
-    // console.log(countries)
     document.querySelector('.option_countries').innerHTML = html
 })
-const fetchDataDetail = async () => {
-    let response = await fetch(apiDetail)
-    let dataDetail = await response.json()
-    return dataDetail;
+
+const countriesPicker = async (country) => {
+    // const optionCountries = document.querySelector('.option_countries')
+    // optionCountries.onchange = function () {
+    // }
+    // country = this.value
+    countryData = country
+    // fetchDataDetail(country)
+    fetchDataCommon(country)
+    fetchDataDetail(countryData).then(dataDetail => {
+        console.log('hello')
+        console.log(dataDetail);
+        if (countryData) {
+            const datas = {
+                labels: ["Infected", "Recovered", "Deaths"]
+                ,
+                datasets: [
+                    {
+                        data: [dataDetail.confirmed.value],
+                        borderColor: '#3333ff',
+                        label: 'Infected',
+                        fill: true,
+                    }, {
+                        data: [dataDetail.deaths.value],
+                        borderColor: 'red',
+                        backgroundColor: "rgba(255, 0, 0, 0.5)",
+                        label: 'Deaths',
+                        fill: true,
+                    }, {
+                        data: [dataDetail.recovered.value],
+                        borderColor: 'green',
+                        backgroundColor: "rgba(0, 255, 0, 0.5)",
+                        label: 'Recovered',
+                        fill: true,
+                    }
+                ]
+            }
+            const config = {
+                type: 'bar',
+                data: datas,
+                options: {}
+            };
+            const covidChart = document.getElementById('covid_chart')
+
+            const covid_chart = new Chart(
+                covidChart, config
+            );
+            // if (covid_chart) {
+            //     covid_chart.destroy()
+            // }
+        } else {
+            const datas = {
+                labels: dataDetail.map(({ dateChecked }) =>
+                    new Date(dateChecked).toLocaleDateString(),
+                ),
+                datasets: [
+                    {
+                        data: dataDetail.map((data) => data.positive),
+                        borderColor: '#3333ff',
+                        label: 'Infected',
+                        fill: true,
+                    }, {
+                        data: dataDetail.map((data) => data.death),
+                        borderColor: 'red',
+                        backgroundColor: "rgba(255, 0, 0, 0.5)",
+                        label: 'Deaths',
+                        fill: true,
+                    }, {
+                        data: dataDetail.map((data) => data.recovered),
+                        borderColor: 'green',
+                        backgroundColor: "rgba(0, 255, 0, 0.5)",
+                        label: 'Recovered',
+                        fill: true,
+                    }
+                ]
+            }
+            const config = {
+                type: 'line',
+                data: datas,
+                options: {}
+            };
+            const covidChart = document.getElementById('covid_chart')
+
+            const covid_chart = new Chart(
+                covidChart, config
+            );
+            // if (covid_chart) {
+            //     covid_chart.destroy()
+            // }
+        }
+    });
+
+
 }
 
-fetchDataDetail().then(dataDetail => {
-    console.log(dataDetail)
-    const datas = {
-        labels: dataDetail.map(({ dateChecked }) =>
-            new Date(dateChecked).toLocaleDateString(),
-        ),
-        datasets: [
-            {
-                data: dataDetail.map((data) => data.positive),
-                borderColor: '#3333ff',
-                label: 'Infected',
-                fill: true,
-            }, {
-                data: dataDetail.map((data) => data.death),
-                borderColor: 'red',
-                backgroundColor: "rgba(255, 0, 0, 0.5)",
-                label: 'Deaths',
-                fill: true,
-            }, {
-                data: dataDetail.map((data) => data.recovered),
-                borderColor: 'green',
-                backgroundColor: "rgba(0, 255, 0, 0.5)",
-                label: 'Recovered',
-                fill: true,
-            }
-        ]
-    };
 
-    const config = {
-        type: 'line',
-        data: datas,
-        options: {}
-    };
-    const covid_chart = new Chart(
-        document.getElementById('covid_chart'),
-        config
-    );
-});
+// countriesPicker();
+
+const fetchDataDetail = async (country) => {
+    if (country) {
+        let response = await fetch(`${apiCommon}/countries/${country}`)
+        let dataDetail = await response.json()
+        console.log(dataDetail)
+
+        return dataDetail
+    } else {
+        let response = await fetch(apiDetail)
+        let dataDetail = await response.json()
+        return dataDetail;
+    }
+}
+
